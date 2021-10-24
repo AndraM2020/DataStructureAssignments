564f110c289d8bf59560330f772bcdf54d37706a
AndraM2020 <andramozga@gmail.com>
2021-10-24 21:09:36 +0300

    Initial commit

--- src/com/company/Assignment2.java
+++ src/com/company/Assignment2.java
@@ -1,6 +1,5 @@
 package com.company;
 
-import java.util.Scanner;
 import java.util.Stack;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
@@ -34,7 +33,6 @@ public class Assignment2 {
         sentences.push("Was it a car or a cat I saw?");
         sentences.push("Madam, in Eden, I’m Adam.");
         sentences.push("Yo, banana boy!");
-        sentences.push("Is Java great or what?");
 
         for (String sent : sentences) {
             System.out.println(sent);
@@ -44,12 +42,11 @@ public class Assignment2 {
                 System.out.println("This sentence is not a palindrome");
             }
         }
-        BlockingQueue<String> sentences1 = new ArrayBlockingQueue<String>(5);
+        BlockingQueue<String> sentences1 = new ArrayBlockingQueue<String>(4);
         sentences1.add("Mom");
         sentences1.add("Was it a car or a cat I saw?");
         sentences1.add("Madam, in Eden, I’m Adam.");
         sentences1.add("Yo, banana boy!");
-        sentences1.add("Is Java great or what?");
 
         for (String sent1 : sentences1) {
             System.out.println(sent1);
@@ -59,26 +56,5 @@ public class Assignment2 {
                 System.out.println("This sentence is not a palindrome");
             }
         }
-
-        System.out.print("Enter any string:");
-        Scanner in = new Scanner(System.in);
-        String inputString = in.nextLine();
-        Stack stack = new Stack();
-
-        for (int i = 0; i < inputString.length(); i++) {
-            stack.push(inputString.charAt(i));
-        }
-
-        String reverseString = "";
-
-        while (!stack.isEmpty()) {
-            reverseString = reverseString + stack.pop();
-        }
-
-        if (inputString.equals(reverseString))
-            System.out.println("The input String is a palindrome.");
-        else
-            System.out.println("The input String is not a palindrome.");
-
     }
 }
